46b048ddacde0228553da7a18e359d8e2dcebac6
Mark Van Akkeren <MarkVanAkkeren@gmail.com>
2017-06-01 09:51:25 -0600

    still running thru postman making sure APIs are working

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -5,7 +5,7 @@ class Api::V1::ItemsController < ApplicationController
   end
 
   def show
-
+    render json: Item.find(params[:id])
   end
 
   def destroy
@@ -13,8 +13,13 @@ class Api::V1::ItemsController < ApplicationController
   end
 
   def create
-
+    render json: Item.create(item_params)
   end
 
+  private
+
+  def item_params
+    params.permit(name:"bob", description:"is a great guy", image_url:"https://pbs.twimg.com/media/Ct29OhhUAAA7DbZ.jpg")
+  end
 
 end
