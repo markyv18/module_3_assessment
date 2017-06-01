ba4cf4409190154c787b649fa23567c461478bb0
Mark Van Akkeren <MarkVanAkkeren@gmail.com>
2017-06-01 10:54:29 -0600

    building tests for API endpoints

---
+++ spec/factories/item.rb
@@ -0,0 +1,7 @@
+FactoryGirl.define do
+  factory :item do
+    name Faker::Name.name
+    description Faker::Lorem.sentence
+    image_url Faker::Avatar.image
+  end
+end
--- spec/requests/api/v1/request_item_spec.rb
+++ spec/requests/api/v1/request_item_spec.rb
@@ -1,24 +1,43 @@
 require 'rails_helper'
 
-RSpec.describe " " do
+RSpec.describe "a user can " do
 
+  it "can GET items" do
+
+    items = create_list(:item, 5)
+
+    get '/api/v1/items'
+
+    expect(response).to be_success
+    expect(response.status).to eq(200)
+
+    response_items = JSON.parse(response.body, symbolize_name: true)
+
+    expect(response_items.count).to eq(5)
+    expect(response_items.first[:id]).to eq(items.first.id)
+    expect(response_items.first[:name]).to eq(items.first.name)
+    expect(response_items.first[:description]).to eq(items.first.description)
+    expect(response_items.first[:image_url]).to eq(items.first.image_url)
+
+    # When I send a GET request to /api/v1/items
+    # I receive a 200 JSON response containing all items
+    # And each item has an id, name, description, image_url
+    # but not the created_at or updated_at
+  end
+
+  it "can GET an item" do
+  #   When I send a GET request to /api/v1/items/1
+  #   I receive a 200 JSON response containing the id, name, description, image_url
+  #   but not the created_at or updated_at
+  end
+
+  it "can DELETE an item" do
+    # When I send a DELETE request to /api/v1/items/1
+    # I receive a 204 JSON response if the record is successfully deleted
+  end
+
+  it "can POST/CREATE an item" do
+    # When I send a POST request to /api/v1/items with a name, description, and image_url
+    # I receive a 201 JSON response if the record is successfully created And I receive a JSON response containing the id, name, description, and image_url but not the created_at or updated_at
+  end
 end
-
-When I send a GET request to /api/v1/items
-I receive a 200 JSON response containing all items
-And each item has an id, name, description, image_url
-but not the created_at or updated_at
-
-When I send a GET request to /api/v1/items/1
-I receive a 200 JSON response containing the id, name, description, image_url
-but not the created_at or updated_at
-
-When I send a DELETE request to /api/v1/items/1
-I receive a 204 JSON response if the record is successfully deleted
-
-When I send a POST request to /api/v1/items with a name, description, and image_url
-I receive a 201 JSON response if the record is successfully created And I receive a JSON response containing the id, name, description, and image_url but not the created_at or updated_at
-
-Verify that your non-GET requests work using Postman or curl.
-(Hint: ActionController::API).
-Why doesn't the default ApplicationController support POST and PUT requests?
